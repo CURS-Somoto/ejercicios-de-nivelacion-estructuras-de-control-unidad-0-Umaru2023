1dc8c454bb52b01ff221e3eecec5edd1e1c67263
Umaru2023 <147261554+Umaru2023@users.noreply.github.com>
2023-10-14 22:10:40 -0600

    Update Ejercicio_5.cs

--- Ejercicio_5.cs
+++ Ejercicio_5.cs
@@ -1,5 +1,28 @@
 /*Ejercicio 5: Positivo o negativo
 
+csharp
+using System;
 
-Escribe un programa en C# que determine si
-un número entero dado es positivo, negativo o cero.*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número entero: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("El número es positivo");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("El número es negativo");
+        }
+        else
+        {
+            Console.WriteLine("El número es cero");
+        }
+
+        Console.ReadLine();
+    }
+}
