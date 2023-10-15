587648618299cfa461f1aa0d57e93a434b412c74
Umaru2023 <147261554+Umaru2023@users.noreply.github.com>
2023-10-14 22:13:39 -0600

    Update Ejercicio_7.cs

--- Ejercicio_7.cs
+++ Ejercicio_7.cs
@@ -1,6 +1,17 @@
 /* Ejercicio 7: Conteo Regresivo
 
+csharp
+using System;
 
-Imprime un conteo regresivo del 10 al 1
-utilizando un bucle for.
-*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 10; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+        
+        Console.ReadLine();
+    }
+}
