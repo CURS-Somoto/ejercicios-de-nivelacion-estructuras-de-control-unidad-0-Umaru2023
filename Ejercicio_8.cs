2a19f979472871fe232d0376fbbfa4ce34f80e69
Umaru2023 <147261554+Umaru2023@users.noreply.github.com>
2023-10-14 22:15:09 -0600

    Update Ejercicio_8.cs

--- Ejercicio_8.cs
+++ Ejercicio_8.cs
@@ -20,3 +20,23 @@ la siguiente secuencia de patrones:
 *****
 
 */
+
+csharp
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
